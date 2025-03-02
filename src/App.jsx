36c2d9e099c01922c721fe90ad338e16a1837bf3
Sumit Mochahary <sumitmochahary5@gmail.com>
2025-03-02 22:28:08 +0530

    Initialize React application with Vite and Tailwind CSS setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import './App.css'
+
+function App() {
+  return (
+    <h1 className='text-9xl font-bold bg-gradient-to-r from-purple-400 via-red-400 to-orange-400 bg-clip-text text-transparent'>Hello World</h1>
+  )
+}
+
+export default App
